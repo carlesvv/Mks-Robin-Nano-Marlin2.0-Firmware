df614bcb51002dd0f7d802b337fb824876139196
TheMacBoy <themacboy72@gmail.com>
2020-08-11 17:30:56 +0200

    Ajustar motors

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -741,7 +741,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.2, 80.4, 401.0, 413.9 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80.2, 80.4, 399.56, 409.80 }
 
 /**
  * Default Max Feed Rate (mm/s)
