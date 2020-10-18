8f364d010b4dfb6a05163d0979a1661927349e8a
TheMacBoy <themacboy72@gmail.com>
2020-10-19 00:41:08 +0200

    afegint suport per MKS UPS (test)

--- Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_V2.h
+++ Marlin/src/pins/stm32f1/pins_MKS_ROBIN_NANO_V2.h
@@ -214,7 +214,7 @@
     #define MKS_TEST_PS_ON_PIN                PB2   // PW_OFF
   #endif
 #else
-  //#define POWER_LOSS_PIN                      PA2   // PW_DET
+  #define POWER_LOSS_PIN                      PA2   // PW_DET
   //#define PS_ON_PIN                           PB2   // PW_OFF
   #define FIL_RUNOUT_PIN                      PA4
   #define FIL_RUNOUT2_PIN                     PE6
