59f784b9b6bc1c89c6be616b6cd31faed0545fbe
TheMacBoy <themacboy72@gmail.com>
2020-11-17 12:30:32 +0100

    error

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -758,7 +758,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320.8, 321.6, 399,6, 1669.33 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 320.8, 321.6, 399.6, 1669.33 }
 
 /**
  * Default Max Feed Rate (mm/s)
