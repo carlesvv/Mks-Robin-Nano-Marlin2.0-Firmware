4c5ebcf59b5f348033a6309f10a127841f5b1a7b
themacboy <themacboy72@gmail.com>
2021-05-05 19:39:40 +0200

    noves mides 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1164,11 +1164,11 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 359
-#define Y_BED_SIZE 343
+#define X_BED_SIZE 377
+#define Y_BED_SIZE 348
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -28
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE/*+X_MIN_POS*/
