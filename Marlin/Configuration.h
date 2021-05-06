7fc19171b5ebb7a445b0fe5d8d1c53d651fa645c
themacboy <themacboy72@gmail.com>
2021-05-06 22:50:48 +0200

    ajust offset Z

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -993,7 +993,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -29.87, 4.159, -01.065} // inductiu
+#define NOZZLE_TO_PROBE_OFFSET { -29.87, 4.159, -01.010} // inductiu
 //#define NOZZLE_TO_PROBE_OFFSET { -58.5316, 5.85755, -2.304} // BL Touch
 
 // Most probes should stay away from the edges of the bed, but
