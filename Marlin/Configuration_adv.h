1f56df516e086a6a3cc2daee302549f671e3f938
themacboy <themacboy72@gmail.com>
2021-05-09 18:15:18 +0200

    test

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2426,7 +2426,7 @@
   #endif
 
   #if AXIS_IS_TMC(E1)
-    #define E1_CURRENT      350
+    #define E1_CURRENT      600
     #define E1_MICROSTEPS    16
     #define E1_RSENSE         0.11
     #define E1_CHAIN_POS     -1
