fe20e9ff0e49ab8938da011db7b4b48bbdc36d06
TheMacBoy <themacboy72@gmail.com>
2021-05-23 21:09:17 +0200

    ajust microsteps de E a 16

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -743,7 +743,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 319.916, 320.712, 1600, 1359.60 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 319.916, 320.712, 1600, 679.8 }
 
 /**
  * Default Max Feed Rate (mm/s)
