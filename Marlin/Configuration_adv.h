858fa8c8e2ab87a3e24dffc9282b16d1c8f37d6d
TheMacBoy <themacboy72@gmail.com>
2021-05-25 20:00:45 +0200

    baixar microsteps de Z a 8

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2384,7 +2384,7 @@
   #if AXIS_IS_TMC(Z)
     #define Z_CURRENT       1600
     #define Z_CURRENT_HOME  Z_CURRENT
-    #define Z_MICROSTEPS     16
+    #define Z_MICROSTEPS     8
     #define Z_RSENSE          0.11
     #define Z_CHAIN_POS      -1
     //#define Z_INTERPOLATE  true
@@ -2393,7 +2393,7 @@
   #if AXIS_IS_TMC(Z2)
     #define Z2_CURRENT      1600
     #define Z2_CURRENT_HOME Z2_CURRENT
-    #define Z2_MICROSTEPS    16
+    #define Z2_MICROSTEPS    8
     #define Z2_RSENSE         0.11
     #define Z2_CHAIN_POS     -1
     //#define Z2_INTERPOLATE true
@@ -2402,7 +2402,7 @@
   #if AXIS_IS_TMC(Z3)
     #define Z3_CURRENT      1600
     #define Z3_CURRENT_HOME Z3_CURRENT
-    #define Z3_MICROSTEPS    16
+    #define Z3_MICROSTEPS    8
     #define Z3_RSENSE         0.11
     #define Z3_CHAIN_POS     -1
     //#define Z3_INTERPOLATE true
@@ -2411,7 +2411,7 @@
   #if AXIS_IS_TMC(Z4)
     #define Z4_CURRENT      1600
     #define Z4_CURRENT_HOME Z4_CURRENT
-    #define Z4_MICROSTEPS    16
+    #define Z4_MICROSTEPS    8
     #define Z4_RSENSE         0.11
     #define Z4_CHAIN_POS     -1
     //#define Z4_INTERPOLATE true
