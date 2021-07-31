98c9b7d31126d74f8ce4c18e1d80bf0f43ff216a
TheMacBoy <themacboy72@gmail.com>
2021-07-31 08:53:01 +0200

    ajustos despres del merge

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1063,8 +1063,7 @@
 #define PROBING_MARGIN 0
 
 // X and Y axis travel speed (mm/min) between probes
-#define XY_PROBE_SPEED (50*60)
-//#define XY_PROBE_FEEDRATE (50*60)
+#define XY_PROBE_FEEDRATE (50*60)
 
 // Feedrate (mm/min) for the first approach when double-probing (MULTIPLE_PROBING == 2)
 #define Z_PROBE_FEEDRATE_FAST (4*60)
@@ -1199,7 +1198,7 @@
 // @section extruder
 
 // For direct drive extruder v9 set to true, for geared extruder set to false.
-#define INVERT_E0_DIR false
+#define INVERT_E0_DIR true
 #define INVERT_E1_DIR false
 #define INVERT_E2_DIR false
 #define INVERT_E3_DIR false
