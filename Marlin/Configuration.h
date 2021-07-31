7563f2bfb5f399789bad89241ce98f5041ee1848
TheMacBoy <themacboy72@gmail.com>
2021-07-31 10:04:37 +0200

    ajustos

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1706,17 +1706,20 @@
 #define PREHEAT_1_LABEL       "PLA"
 #define PREHEAT_1_TEMP_HOTEND 160
 #define PREHEAT_1_TEMP_BED     70
-#define PREHEAT_1_FAN_SPEED     64 // Value from 0 to 255
+#define PREHEAT_1_TEMP_CHAMBER 35
+#define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_2_LABEL       "ABS"
 #define PREHEAT_2_TEMP_HOTEND 220
 #define PREHEAT_2_TEMP_BED    80
+#define PREHEAT_2_TEMP_CHAMBER 35
 #define PREHEAT_2_FAN_SPEED     0 // Value from 0 to 255
 
 #define PREHEAT_3_LABEL       "PETG"
 #define PREHEAT_3_TEMP_HOTEND 180
 #define PREHEAT_3_TEMP_BED    80
-#define PREHEAT_3_FAN_SPEED     64 // Value from 0 to 255
+#define PREHEAT_3_TEMP_CHAMBER 35
+#define PREHEAT_3_FAN_SPEED     0 // Value from 0 to 255
 
 /**
  * Nozzle Park
