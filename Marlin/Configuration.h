6021feb474b86f6bd7751377e77df7acb60bcdf0
TheMacBoy <themacboy72@gmail.com>
2021-08-10 17:31:24 +0200

    canvi de varilles roscades a 2mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -806,18 +806,18 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 159.958, 160.356, 800, 693.673 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 159.958, 160.356, 1600, 693.673 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 15, 60 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 60 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
-  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 15, 60 } // ...or, set your own edit limits
+  #define MAX_FEEDRATE_EDIT_VALUES    { 200, 200, 5, 60 } // ...or, set your own edit limits
 #endif
 
 /**
